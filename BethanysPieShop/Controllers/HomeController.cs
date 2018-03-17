b3beef0ddfe5fa401322485267135eaee15dfcbb
Rajnish Jha <rajha@apttus.com>
2018-03-17 18:55:03 +0530

    BethanysPieShop Project Added

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+
+namespace BethanysPieShop.Controllers {
+    public class HomeController : Controller {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository) {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index() {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel() {
+                Pies = pies.ToList(),
+                Title = "Welcome to Bethany's Pie Shop"
+            };
+
+            return View(homeViewModel);
+        }
+
+        public IActionResult Details(int id) {
+            var pie = _pieRepository.GetPieById(id);
+            if(pie == null)
+                return NotFound();
+
+            return View(pie);
+        }
+    }
+}
