24b28d9768fbc9a70344a6609a9df36ceb27ab31
Rajnish Jha <rajha@apttus.com>
2018-03-17 18:58:33 +0530

    Course exercise files added

---
+++ Exercise Files/04/demos/Demos/End/BethanysPieShop/BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Pies = pies.ToList(),
+                Title = "Welcome to Bethany's Pie Shop"
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
